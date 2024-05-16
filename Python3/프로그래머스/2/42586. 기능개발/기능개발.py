4bb8d1552966d26cd1904d341e8d39a9497701e3
Haeun Park <haeun8701@gmail.com>
2024-05-16 13:41:44 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+def solution(progresses, speeds):
+    answer = []
+
+    queue = deque(progresses)
+    length = len(progresses)
+
+    cnt = 1
+    for i in range(length):
+        pre_cnt = cnt
+        while progresses[i] + (speeds[i] * cnt) < 100:
+            cnt += 1
+        next_cnt = cnt
+        if pre_cnt != next_cnt:
+            answer.append(1)
+        else:
+            answer[-1] += 1
+        queue.popleft()
+
+    return answer
+
+if __name__ == '__main__':
+    print(solution(	[93, 30, 55], [1, 30, 5]))
