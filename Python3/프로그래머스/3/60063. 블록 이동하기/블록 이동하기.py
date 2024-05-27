04a1875fc0d8b4afd3686edc1446531adbad6ae8
Haeun Park <haeun8701@gmail.com>
2024-05-27 18:56:08 +0900

    [level 3] Title: 블록 이동하기, Time: 39.01 ms, Memory: 11.3 MB -BaekjoonHub

--- Python3/프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
+++ Python3/프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
@@ -2,9 +2,12 @@ from collections import deque
 
 def solution(board):
     
-    dr = [0, 1,  0, -1]
-    dc = [1, 0, -1,  0]
+    one_dr = [0, 1,  0, -1,  0,  0,  0,  0, 1, -1,  1, 1]
+    one_dc = [1, 0, -1,  0,  0,  0,  0,  0, 1,  1, -1, 1]
+    two_dr = [0, 1,  0, -1, -1,  1, -1, -1, 0,  0,  0, 0]
+    two_dc = [1, 0, -1,  0, -1, -1,  1, -1, 0,  0,  0, 0 ]
     n = len(board)
+    sec = 100 * 100
     
     def in_range(r1, c1, r2, c2):
         return 0 <= r1 < n and 0 <= c1 < n and 0 <= r2 < n and 0 <= c2 < n
@@ -13,43 +16,44 @@ def solution(board):
     visited = set()
     q.append((0, 0, 0, 1, 0))
     visited.add((0, 0, 0, 1))
-    
     while q:
         cur_one_r, cur_one_c, cur_two_r, cur_two_c, cur_sec = q.popleft()
-        
-        if cur_one_r == n-1 and cur_one_c == n-1 or cur_two_r == n-1 and cur_two_c == n-1:
-            return cur_sec
-        
-        for i in range(4):
-            nor, noc, ntr, ntc = cur_one_r + dr[i], cur_one_c + dc[i], cur_two_r + dr[i], cur_two_c + dc[i]
-            if in_range(nor, noc, ntr, ntc) and (nor, noc, ntr, ntc) not in visited \
-            and board[nor][noc] == 0 and board[ntr][ntc] == 0:
-                q.append((nor, noc, ntr, ntc, cur_sec + 1))
-                visited.add((nor, noc, ntr, ntc))
-    
-        if cur_one_r == cur_two_r : # 가로일 때
-            for j in (-1, 1):
-                nor, ntr = cur_one_r + j, cur_two_r + j
-                if in_range(nor, cur_one_c, ntr, cur_two_c) and board[nor][cur_one_c] == 0 and board[ntr][cur_two_c] == 0:
-                    if (cur_one_r, cur_one_c, nor, cur_one_c) not in visited:
-                        q.append((cur_one_r, cur_one_c, nor, cur_one_c, cur_sec + 1)) 
-                        visited.add((cur_one_r, cur_one_c, nor, cur_one_c))
-                    if (ntr, cur_two_c, cur_two_r, cur_two_c) not in visited:
-                        q.append((ntr, cur_two_c, cur_two_r, cur_two_c, cur_sec + 1)) 
-                        visited.add((ntr, cur_two_c, cur_two_r, cur_two_c))
-                        
-        elif cur_one_c == cur_two_c: # 세로일 때
-            for j in (-1, 1):
-                noc, ntc = cur_one_c + j, cur_two_c + j
-                if in_range(cur_one_r, noc, cur_two_r, ntc) and board[cur_one_r][noc] == 0 and board[cur_two_r][ntc] == 0:
-                    if (cur_one_r, cur_one_c, cur_one_r, noc) not in visited:
-                        q.append((cur_one_r, cur_one_c, cur_one_r, noc, cur_sec + 1))
-                        visited.add((cur_one_r, cur_one_c, cur_one_r, noc))
-                    if (cur_two_r, ntc, cur_two_r, cur_two_c) not in visited:    
-                        q.append((cur_two_r, ntc, cur_two_r, cur_two_c, cur_sec + 1)) 
-                        visited.add((cur_two_r, ntc, cur_two_r, cur_two_c))
+        for i in range(12):
+            if cur_one_r != cur_two_r and i in (4, 5, 8, 9): # 가로 회전일 때 가로가 아니라면
+                continue
+            elif cur_one_c != cur_two_c and i in (6, 7, 10, 11): # 세로 회전일 때 세로가 아니라면 
+                continue
+            next_one_r = cur_one_r + one_dr[i]
+            next_one_c = cur_one_c + one_dc[i]
+            next_two_r = cur_two_r + two_dr[i]
+            next_two_c = cur_two_c + two_dc[i]
+            if i == 4 or i == 5:
+                if in_range(next_one_r, next_one_c + 1, next_two_r, next_two_c + 1):
+                    if board[next_one_r][next_one_c + 1] and board[next_two_r][next_two_c + 1]:
+                        continue
+            elif i == 6 or i == 7:
+                if in_range(next_one_r - 1, next_one_c, next_two_r - 1, next_two_c):
+                    if board[next_one_r - 1][next_one_c] and board[next_two_r - 1][next_two_c]:
+                        continue
+            elif i == 8 or i == 9:
+                if in_range(next_one_r, next_one_c - 1, next_two_r, next_two_c - 1):
+                    if board[next_one_r][next_one_c - 1] and board[next_two_r][next_two_c - 1]:
+                        continue
+            elif i == 10 or i == 11:
+                if in_range(next_one_r + 1, next_one_c, next_two_r + 1, next_two_c):
+                    if board[next_one_r + 1][next_one_c] and board[next_two_r + 1][next_two_c]:
+                        continue
 
-    return -1
+            if in_range(next_one_r, next_one_c, next_two_r, next_two_c):
+                if (next_one_r, next_one_c, next_two_r, next_two_c) not in visited:
+                    if board[next_one_r][next_one_c] == 0 and board[next_two_r][next_two_c] == 0:
+                        q.append((next_one_r, next_one_c,   next_two_r, next_two_c, cur_sec + 1))
+                        visited.add((next_one_r, next_one_c, next_two_r, next_two_c))
+                        if (next_one_r == n - 1 and next_one_c == n - 1) or (next_two_r == n - 1 and next_two_c == n - 1):
+                            sec = min(sec, cur_sec + 1)
+                            
+
+    return sec
 
 board = [[0, 0, 0, 1, 1], [0, 0, 0, 1, 0], [0, 1, 0, 1, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0]]
 print(solution(board))
