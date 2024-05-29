91250caf1641b33323d816c4fc43743b59724d00
Haeun Park <haeun8701@gmail.com>
2024-05-29 16:33:01 +0900

    [level 3] Title: 블록 이동하기, Time: 52.80 ms, Memory: 11.3 MB -BaekjoonHub

--- Python3/프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
+++ Python3/프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
@@ -1,59 +1,44 @@
 from collections import deque
 
 def solution(board):
-    
-    one_dr = [0, 1,  0, -1,  0,  0,  0,  0, 1, -1,  1, 1]
-    one_dc = [1, 0, -1,  0,  0,  0,  0,  0, 1,  1, -1, 1]
-    two_dr = [0, 1,  0, -1, -1,  1, -1, -1, 0,  0,  0, 0]
-    two_dc = [1, 0, -1,  0, -1, -1,  1, -1, 0,  0,  0, 0 ]
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
     n = len(board)
-    sec = 100 * 100
     
-    def in_range(r1, c1, r2, c2):
-        return 0 <= r1 < n and 0 <= c1 < n and 0 <= r2 < n and 0 <= c2 < n
-            
-    q = deque()
-    visited = set()
-    q.append((0, 0, 0, 1, 0))
-    visited.add((0, 0, 0, 1))
+    def in_range(r, c):
+        return 0 <= r < n and 0 <= c < n
+    
+    def can_move(r1, c1, r2, c2):
+        return in_range(r1, c1) and in_range(r2, c2) and board[r1][c1] == 0 and board[r2][c2] == 0
+    
+    def add_state(q, visited, r1, c1, r2, c2, sec):
+        if ((r1, c1, r2, c2) not in visited):
+            q.append((r1, c1, r2, c2, sec))
+            visited.add((r1, c1, r2, c2))
+
+    q = deque([(0, 0, 0, 1, 0)])
+    visited = set([(0, 0, 0, 1)])
+    
     while q:
-        cur_one_r, cur_one_c, cur_two_r, cur_two_c, cur_sec = q.popleft()
-        for i in range(12):
-            if cur_one_r != cur_two_r and i in (4, 5, 8, 9): # 가로 회전일 때 가로가 아니라면
-                continue
-            elif cur_one_c != cur_two_c and i in (6, 7, 10, 11): # 세로 회전일 때 세로가 아니라면 
-                continue
-            next_one_r = cur_one_r + one_dr[i]
-            next_one_c = cur_one_c + one_dc[i]
-            next_two_r = cur_two_r + two_dr[i]
-            next_two_c = cur_two_c + two_dc[i]
-            if i == 4 or i == 5:
-                if in_range(next_one_r, next_one_c + 1, next_two_r, next_two_c + 1):
-                    if board[next_one_r][next_one_c + 1] and board[next_two_r][next_two_c + 1]:
-                        continue
-            elif i == 6 or i == 7:
-                if in_range(next_one_r - 1, next_one_c, next_two_r - 1, next_two_c):
-                    if board[next_one_r - 1][next_one_c] and board[next_two_r - 1][next_two_c]:
-                        continue
-            elif i == 8 or i == 9:
-                if in_range(next_one_r, next_one_c - 1, next_two_r, next_two_c - 1):
-                    if board[next_one_r][next_one_c - 1] and board[next_two_r][next_two_c - 1]:
-                        continue
-            elif i == 10 or i == 11:
-                if in_range(next_one_r + 1, next_one_c, next_two_r + 1, next_two_c):
-                    if board[next_one_r + 1][next_one_c] and board[next_two_r + 1][next_two_c]:
-                        continue
+        r1, c1, r2, c2, sec = q.popleft()
+        
+        if (r1 == n-1 and c1 == n-1) or (r2 == n-1 and c2 == n-1):
+            return sec
+        
+        for i in range(4):
+            nr1, nc1, nr2, nc2 = r1 + dr[i], c1 + dc[i], r2 + dr[i], c2 + dc[i]
+            if can_move(nr1, nc1, nr2, nc2):
+                add_state(q, visited, nr1, nc1, nr2, nc2, sec + 1)
+        
+        if r1 == r2:
+            for d in (-1, 1):
+                if in_range(r1 + d, c1) and in_range(r2 + d, c2) and board[r1 + d][c1] == 0 and board[r2 + d][c2] == 0:
+                    add_state(q, visited, r1, c1, r1 + d, c1, sec + 1)
+                    add_state(q, visited, r2, c2, r2 + d, c2, sec + 1)
+        elif c1 == c2:
+            for d in (-1, 1):
+                if in_range(r1, c1 + d) and in_range(r2, c2 + d) and board[r1][c1 + d] == 0 and board[r2][c2 + d] == 0:
+                    add_state(q, visited, r1, c1, r1, c1 + d, sec + 1)
+                    add_state(q, visited, r2, c2, r2, c2 + d, sec + 1)
 
-            if in_range(next_one_r, next_one_c, next_two_r, next_two_c):
-                if (next_one_r, next_one_c, next_two_r, next_two_c) not in visited:
-                    if board[next_one_r][next_one_c] == 0 and board[next_two_r][next_two_c] == 0:
-                        q.append((next_one_r, next_one_c,   next_two_r, next_two_c, cur_sec + 1))
-                        visited.add((next_one_r, next_one_c, next_two_r, next_two_c))
-                        if (next_one_r == n - 1 and next_one_c == n - 1) or (next_two_r == n - 1 and next_two_c == n - 1):
-                            sec = min(sec, cur_sec + 1)
-                            
-
-    return sec
-
-board = [[0, 0, 0, 1, 1], [0, 0, 0, 1, 0], [0, 1, 0, 1, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0]]
-print(solution(board))
+    return -1
