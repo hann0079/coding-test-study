155649d3abf7461786639e5ff15258a125c60581
Haeun Park <haeun8701@gmail.com>
2025-03-28 23:39:43 +0900

    [level 2] Title: 기능개발, Time: 0.48 ms, Memory: 9.26 MB -BaekjoonHub

--- Python3/프로그래머스/2/42586. 기능개발/기능개발.py
+++ Python3/프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,16 +1,26 @@
 from collections import deque
-
 def solution(progresses, speeds):
+    #✅ 진도 배열의 값이 들어간 큐를 생성한다.
+    q = deque(progresses)
+    #✅ 속도 배열의 값이 들어간 큐를 생성한다.
+    s = deque(speeds)
+    #✅ 정답을 저장할 빈 리스트를 만든다.
     answer = []
-    progresses = deque(progresses)
-    speeds = deque(speeds)
-    while progresses :
+    #✅ 진도(속도) 큐에 값이 있는 동안 반복한다.
+    while q:
+        #✅ 진도 큐에 속도 큐의 값을 각각 더한다.
+        for i in range(len(q)):
+            q[i] += s[i]
+        #✅ 배포 개수를 저장할 변수를 초기화한다.
         cnt = 0
-        while progresses and progresses[0] >= 100:
-            cnt+=1
-            progresses.popleft()
-            speeds.popleft()
-        progresses = deque([progresses[i]+speeds[i] for i in range(len(progresses))])
+        #✅ 진도 큐의 front가 100 이상인 동안 반복한다.
+        while q and q[0] >= 100:
+            #✅ 진도 큐와 속도 큐에서 값을 꺼낸다.
+            q.popleft()
+            s.popleft()
+            #✅ count 변수를 1 증가한다.
+            cnt += 1
+        #✅ count 변수의 값이 0이 아니라면 정답 리스트에 추가한다.
         if cnt:
             answer.append(cnt)
     
