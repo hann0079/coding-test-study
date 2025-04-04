c3c5eb221260a53bc7514f40e55f73d970be279e
Haeun Park <haeun8701@gmail.com>
2025-04-04 09:30:04 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- Python3/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ Python3/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,19 +1,20 @@
 from collections import deque
 
 def solution(cards1, cards2, goal):
-    
-    cards1_q = deque(cards1)
-    cards2_q = deque(cards2)
-    goal_q = deque(goal)
-    
-    while goal_q:
-        if cards1_q and goal_q[0] == cards1_q[0]:
-            cards1_q.popleft()
-            goal_q.popleft()
-        elif cards2_q and goal_q[0] == cards2_q[0]:
-            cards2_q.popleft()
-            goal_q.popleft()
+    # cards와 goal을 deque로 변환
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+    goal = deque(goal)
+
+    # ➊ goal의 문자열을 순차적으로 순회
+    while goal:
+        if cards1 and cards1[0] == goal[0]:  # ➋ card1의 front와 일치하는 경우
+            cards1.popleft()
+            goal.popleft()
+        elif cards2 and cards2[0] == goal[0]:  # ➌ card2의 front와 일치하는 경우
+            cards2.popleft()
+            goal.popleft()
         else:
-            break
-    
-    return "Yes" if not goal_q else "No"
+            break  # 일치하는 원소를 찾지 못했으므로 종료
+
+    return "Yes" if not goal else "No"  # ➍ goal이 비었으면 “Yes” 아니면 “No”를 반환
