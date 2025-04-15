dcb547ca577b0c09d713249c226a3c5ae13ee77f
Haeun Park <haeun8701@gmail.com>
2025-04-15 21:11:20 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 9.29 MB -BaekjoonHub

--- Python3/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ Python3/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,7 +1,3 @@
 def solution(s1, s2):
-    answer = 0
-    for item1 in s1:
-        for item2 in s2:
-            if item1 == item2:
-                answer += 1
-    return answer
+    answer = set(s1).intersection(set(s2))
+    return len(answer)
