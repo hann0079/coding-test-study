e2bbc533545c852cb2940cd2d29e196d8aea75df
Haeun Park <haeun8701@gmail.com>
2025-06-05 21:39:20 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 35144 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+시험성적 = int(input())
+if 90 <= 시험성적 <= 100:
+    print('A')
+elif 80 <= 시험성적 <= 89:
+    print('B')
+elif 70 <= 시험성적 <= 79:
+    print('C')
+elif 60 <= 시험성적 <= 69:
+    print('D')
+else:
+    print('F')
