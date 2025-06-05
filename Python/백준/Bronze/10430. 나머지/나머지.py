410a4d25f30a0cebaf0d6759b5d3cd845d5771bd
Haeun Park <haeun8701@gmail.com>
2025-06-05 21:13:02 +0900

    [Bronze V] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/10430. 나머지/나머지.py
+++ Python/백준/Bronze/10430. 나머지/나머지.py
@@ -1,2 +1,5 @@
 A, B, C = map(int, input().split())
-print((A+B)%C, ((A%C) + (B%C))%C, (A*B)%C, ((A%C) * (B%C))%C, sep='\n')
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
