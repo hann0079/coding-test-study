cdd9292ce3caf468794fdb0a9c26b9f42721e281
Haeun Park <haeun8701@gmail.com>
2025-06-10 22:13:14 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- Python3/프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ Python3/프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,7 +1,8 @@
 def solution(n):
+    # answer = n // 7
+    # return answer + 1 if n % 7 else answer
+
     p = 1
-    while n // (p * 7) > 1: 
-        p += 1
-    while p * 7 < n:
+    while n // (p * 7) >= 1 and n % (p * 7):
         p += 1
     return p
