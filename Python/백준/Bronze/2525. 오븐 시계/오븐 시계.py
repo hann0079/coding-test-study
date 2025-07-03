f158f9c4aa1e55060800b4c89405ee7c6320233a
Haeun Park <haeun8701@gmail.com>
2025-07-03 20:49:43 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ Python/백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,11 +1,5 @@
 h, m = map(int, input().split())
 t = int(input())
 
-result = m + t 
-while result >= 60:
-    h += 1
-    if h == 24:
-        h = 0
-    result -= 60
-
-print(h, result)
+q, r = divmod(m+t, 60)
+print((h+q) % 24, r)
