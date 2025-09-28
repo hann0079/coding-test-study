322d5173ea7724b23d2502223549461ca03c6fd0
Haeun Park <haeun8701@gmail.com>
2025-09-28 21:38:36 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 9.44 MB -BaekjoonHub

--- Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,6 @@
 def solution(s):
     str = list(map(int, s.split()))
     answer = "%d %d" % (min(str),max(str))
+    # answer = f"{min(str), max(str)}"
+    # answer = str(min(str)) + " " + str(max(str))
     return answer
