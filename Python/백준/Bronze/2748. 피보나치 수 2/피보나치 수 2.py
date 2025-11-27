bbd56281a89a8de986649c8963b5fadc66f6f1d1
Haeun Park <haeun8701@gmail.com>
2025-11-27 10:31:36 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ Python/백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,19 +1,14 @@
 import sys
 
-dp = [0] * 100 
-
-def fibo(x):
-    if x == 0:
-        return 0
-    if x == 1:
-        return 1
-    
-    if dp[x] != 0:
-        return dp[x]
-    
-    dp[x] = fibo(x - 1) + fibo(x - 2)
-    return dp[x]
-
-
 n = int(sys.stdin.readline())
-print(fibo(n))
+
+dp = [0] * (n + 1)
+
+dp[0] = 0
+if n > 0:
+    dp[1] = 1
+
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[n])
