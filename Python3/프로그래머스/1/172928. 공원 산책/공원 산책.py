760af771677d2abb6f86978ffb4ce1d997c8b248
Haeun Park <haeun8701@gmail.com>
2025-11-30 22:18:20 +0900

    [level 1] Title: 공원 산책, Time: 0.11 ms, Memory: 9.32 MB -BaekjoonHub

--- Python3/프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ Python3/프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,26 +1,41 @@
 def solution(park, routes):
-    nav = {
-        'S': (1,0),
-        'N': (-1, 0),
-        'W': (0, -1),
-        'E': (0,1)        
-    }
-    h, w = len(park), len(park[0])
-    for i in range(h):
-        for j in range(w):
-            if park[i][j] == 'S':
-                x, y = i, j
-    
-    for route in routes:
-        direction, distance = route[0], int(route[2])
-        next_x, next_y = x, y
-        dx, dy = nav[direction][0], nav[direction][1]
-        for i in range(distance):
-            next_x += dx
-            next_y += dy
-            if not (0 <= next_x < h) or not (0 <= next_y < w) or park[next_x][next_y] == 'X':
-                break
-        else:
-            x, y = next_x, next_y
-        
-    return [x, y]
+    answer = []
+    Up=len(park)
+    Side=len(park[0])
+    for i in range(len(park)):
+        if 'S' in park[i]:
+            y=park[i].index('S')
+            x=i
+
+    for j in range(len(routes)):
+        direction,distance=routes[j][0],int(routes[j][2])
+        if direction=='S':
+            if x+distance<Up:
+                for i in range(1, distance+1):
+                    if park[x+i][y]=='X':
+                        break
+                else:
+                    x+=distance
+        elif direction=='W':
+            if y-distance>=0:
+                for i in range(1, distance+1):
+                    if park[x][y-i]=='X':
+                        break
+                else:
+                    y-=distance
+        elif direction=='E':
+            if y+distance<Side:
+                for i in range(1, distance+1):
+                    if park[x][y+i]=='X':
+                        break
+                else:
+                    y+=distance
+        elif direction=='N':
+            if x-distance>=0:
+                for i in range(1, distance+1):
+                    if park[x-i][y]=='X':
+                        break 
+                else: # 반복 정상 종료 시 실행 (break로 강제 종료 X)
+                    x -= distance
+    answer=[x,y]
+    return answer
